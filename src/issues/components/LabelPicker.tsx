d51aec7357e0f4e59284e614bbc0a78c29a5769b
MiGaNg <miguelangelyv1@gmail.com>
2025-11-05 10:17:36 -0500

    feat: Integrate React Query for GitHub API data fetching and add label management functionality

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,12 +1,42 @@
+import { useQuery } from "@tanstack/react-query";
+import { getLabels } from "../actions";
+
+
 export const LabelPicker = () => {
+  const { data: labels, isLoading } = useQuery({
+    queryKey: ['labels'],
+    queryFn: getLabels
+  })
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-52">
+        Espere ...
+      </div>
+    )
+  }
+
+
+
   return (
-    <>
+    <div className="flex flex-wrap justify-center gap-2">
+      {
+        labels?.map(label => (
+          <span
+            key={label.id}
+            className="px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer"
+            style={{ border: `1px solid #${label.color}`, color: `#${label.color}` }}
+          >
+            {label.name}
+          </span>
+        ))
+      }
       <span
         className="px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer"
         style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
       >
         Primary
       </span>
-    </>
+    </div>
   );
 };
---
+++ src/issues/types/labelType.ts
@@ -0,0 +1,10 @@
+export interface GithubLabel {
+    id: number;
+    node_id: string;
+    url: string;
+    name: string;
+    color: string;
+    default: boolean;
+    description?: string;
+}
+
