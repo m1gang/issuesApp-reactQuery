1d13624bda7884221491c5a8e8330af2bcee3330
MiGaNg <miguelangelyv1@gmail.com>
2025-11-05 11:04:27 -0500

    initialData y placeholderData

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -29,12 +29,7 @@ export const LabelPicker = () => {
           </span>
         ))
       }
-      <span
-        className="px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer"
-        style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-      >
-        Primary
-      </span>
+
     </div>
   );
 };
--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -1,11 +1,32 @@
 import { useQuery } from '@tanstack/react-query'
 import { getLabels } from '../actions'
+import { GithubLabel } from '../types'
 
 export const useLabels = () => {
     return useQuery({
         queryKey: ['labels'],
         queryFn: getLabels,
         staleTime: 1000 * 60 * 60, // 1 hora en la que se va considerar fresco, es decir, no volvera a hacer fetch
+        placeholderData: [
+            {
+                "id": 1109410193,
+                "node_id": "MDU6TGFiZWwxMTA5NDEwMTkz",
+                "url": "https://api.github.com/repos/facebook/react/labels/Component:%20Hooks",
+                "name": "Component: Hooks",
+                "color": "c2f27b",
+                "default": false,
+            } satisfies GithubLabel,
+        ],
+        // initialData: [
+        //     {
+        //         "id": 1109410193,
+        //         "node_id": "MDU6TGFiZWwxMTA5NDEwMTkz",
+        //         "url": "https://api.github.com/repos/facebook/react/labels/Component:%20Hooks",
+        //         "name": "Component: Hooks",
+        //         "color": "c2f27b",
+        //         "default": false,
+        //     } satisfies GithubLabel,
+        // ]
     })
 
 }
